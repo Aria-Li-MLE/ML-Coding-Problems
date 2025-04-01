613ecaf44f040e01cc558ad8ea19e4d4289fe7c9
Aria Li <ariali@Arias-MacBook-Air.local>
2025-03-31 19:15:26 -0700

    add comments

--- EvaluationMetrics/binary_confusion_matrix.py
+++ EvaluationMetrics/binary_confusion_matrix.py
@@ -1,5 +1,21 @@
 def binary_confusion_matrix(y_true,y_pred):
-
+    """
+    Calculate confusion matrix for binary classification problems
+    
+    Args:
+        y_true (list): List of true labels (0s and 1s)
+        y_pred (list): List of predicted labels (0s and 1s)
+        
+    Returns:
+        list: 2x2 confusion matrix as [[true_positives, false_negatives],
+                                     [false_positives, true_negatives]]
+    
+    Example:
+        >>> y_true = [1, 0, 1, 1, 0, 0]
+        >>> y_pred = [1, 0, 0, 1, 1, 0]
+        >>> binary_confusion_matrix(y_true, y_pred)
+        [[2, 1], [1, 2]]
+    """
     # Initialize counters
     tp = tn = fp = fn = 0
 
@@ -15,7 +31,6 @@ def binary_confusion_matrix(y_true,y_pred):
             tn += 1
 
     # Return as 2x2 matrix       
-    cf = [[tp, fn],
-          [fp,tn]]
-    return cf
+    return [[tp, fn],
+            [fp, tn]]
 
